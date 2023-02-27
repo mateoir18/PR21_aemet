d838078bfcfba8d5488f3d498a6af0a595ba344e
User <User@LAPTOP-FHIH4P5M>
2023-02-27 22:39:51 +0100

    Ejercicio de prueba

---
+++ src/PruebaGit/Start.java
@@ -0,0 +1,20 @@
+package PruebaGit;
+
+public class Start {
+
+	public static void main(String[] args) {
+		
+		 int numero;
+		 numero = (int)(Math.random()*100);
+		 System.out.println(numero);
+		 
+		 if(numero>50) {
+			 System.out.println("El número es mayor de 50");
+		 }
+		 else {
+			 System.out.println("el número es menor de 50");
+		 }
+
+	}
+
+}
